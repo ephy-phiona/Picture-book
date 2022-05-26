f3b27e8af4b5954d0469f4c30bcc5117c5753377
ephy-phiona <ephyphiona@gmail.com>
2022-05-26 09:57:03 +0300

    changes

--- app/src/main/java/dev/phiona/book/PictureBook3.kt
+++ app/src/main/java/dev/phiona/book/PictureBook3.kt
@@ -6,18 +6,18 @@ import android.os.Bundle
 import android.widget.Button
 
 class PictureBook3 : AppCompatActivity() {
-    lateinit var btnBackkk3:Button
-    lateinit var btnNextt3:Button
+    lateinit var btnBacck3:Button
+    lateinit var btnNexttt3:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture_book3)
-        btnBackkk3=findViewById(R.id.btnBackkk3)
-        btnBackkk3.setOnClickListener {
+        btnBacck3=findViewById(R.id.btnBackkk3)
+        btnBacck3.setOnClickListener {
             val intent= Intent(this,PictureBook2::class.java)
             startActivity(intent)
         }
-        btnNextt3=findViewById(R.id.btnNextt3)
-        btnNextt3.setOnClickListener {
+        btnNexttt3=findViewById(R.id.btnNextt3)
+        btnNexttt3.setOnClickListener {
             val intent= Intent(this,Picturebook4::class.java)
             startActivity(intent)
         }
